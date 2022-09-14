e9a69a577a7129ba16376f161c8625e2a4c804c5
AngeloGarante <angelogarante@outlook.com>
2022-09-14 16:43:15 +0200

    First Update Pokemon List

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
-let myName="angelo";
-document.write(myName);
+let pokemonList=[
+   Gengar={
+    height:"1.5 m",
+    type:"shadow/poison",
+    weight:"40.5 kg",
+},
+   Snorlax={
+    height:"2.10 m",
+    type:"normal",
+    weight:"450 kg",
+},
+   Magmar={
+    height:"1.3 m",
+    type:"fire",
+    weight:"44.5 kg",
+   }
+];
