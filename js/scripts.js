efa88de62a8cbd3f0b364c0d151d45683d9f52b7
AngeloGarante <angelogarante@outlook.com>
2022-09-15 17:20:05 +0200

    update exercise 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,31 @@
-let pokemonList=[
-   Gengar={
-    height:"1.5 m",
-    type:"shadow/poison",
-    weight:"40.5 kg",
-},
-   Snorlax={
-    height:"2.10 m",
-    type:"normal",
-    weight:"450 kg",
-},
-   Magmar={
-    height:"1.3 m",
-    type:"fire",
-    weight:"44.5 kg",
-   }
-];
+let pokemonList = [
+    {//height values meters , weight value kg.
+        name: "Gengar",
+        height: 1.5,
+        type: "shadow/poison",
+        weight: 40.5,
+    },
+    {
+        name: "Snorlax",
+        height: 2.10,
+        type: "normal",
+        weight: "450 kg",
+    },
+    {
+        name: "magmar",
+        height: 1.3,
+        type: "fire",
+        weight: 44.5,
+    }
+]
+
+for (i = 0; i < pokemonList.length; i++) {
+    const pokemon = pokemonList[i]
+    const height = pokemon.height
+    /*  if (height > 1.7) {
+          document.write(`${pokemon.name} (height ${height} meter) this guys is a big one <br>`)
+      }
+      else { document.write(`${pokemon.name} (height ${height})<br>`) }
+      */
+    document.write(`${pokemon.name} (height ${height} meter) ${height > 1.7 ? "this guys is a big one" : ""} <br>`)
+}
