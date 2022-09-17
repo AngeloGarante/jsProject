9173ebea7ccff86683f97a0082533e62bd92b2af
AngeloGarante <angelogarante@outlook.com>
2022-09-17 12:19:47 +0200

    Error Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,36 @@
-let pokemonList = [
-    {//height values meters , weight value kg.
-        name: "Gengar",
-        height: 1.5,
-        type: "shadow/poison",
-        weight: 40.5,
-    },
-    {
-        name: "Snorlax",
-        height: 2.10,
-        type: "normal",
-        weight: "450 kg",
-    },
-    {
-        name: "magmar",
-        height: 1.3,
-        type: "fire",
-        weight: 44.5,
-    }
-]
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {//height values meters , weight value kg.
+            name: "Gengar",
+            height: 1.5,
+            type: "shadow/poison",
+            weight: 40.5,
+        },
+        {
+            name: "Snorlax",
+            height: 2.10,
+            type: "normal",
+            weight: "450 kg",
+        },
+        {
+            name: "magmar",
+            height: 1.3,
+            type: "fire",
+            weight: 44.5,
+        }
+    ];
+    function add(item) {
+        pokemonList.push(item);
 
-for (i = 0; i < pokemonList.length; i++) {
-    const pokemon = pokemonList[i]
-    const height = pokemon.height
-    /*  if (height > 1.7) {
-          document.write(`${pokemon.name} (height ${height} meter) this guys is a big one <br>`)
-      }
-      else { document.write(`${pokemon.name} (height ${height})<br>`) }
-      */
-    document.write(`${pokemon.name} (height ${height} meter) ${height > 1.7 ? "this guys is a big one" : ""} <br>`)
-}
+    }
+    function getAll() {
+        return this.pokemonList;
+    }
+});
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write(`name: ${pokemon.name} (height: ${pokemon.height} meter) ${pokemon.height > 1.7 ? "wow this pokemon is really big!" : ""} <br>`);
+
+});
+
+
